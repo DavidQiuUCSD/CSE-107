6922635c294f56fce58dc1dd8403f6583f3f94e1
David Qiu <zhq028@eng.ucsd.edu>
2017-01-27 22:01:56 -0800

    Add files via upload

---
+++ Discussions/Week 3/discussion3.py
@@ -0,0 +1,55 @@
+from crypto.tools import split, xor_strings
+from crypto.ideal.block_cipher import BlockCipher
+
+# Block & key size in bytes.
+block_len = 16 #  == k above
+key_len = 16   #  == k above
+
+#PRF F
+F = BlockCipher(block_len, key_len).encrypt
+
+"""
+G1 and G2 are the first two attempted PRF constructions on the last PRF slide.
+Try to write attacks against them in A_1 and A_2.
+"""
+
+
+def G1(k, x):
+    xbar = xor_strings(x, "\xFF" * block_len)
+    
+    y1 = F(k,x)
+    y2 = F(k,xbar)
+
+    return y1 + y2    
+
+def A_1(fn):
+
+    pass
+
+def G2(k, x):
+    y1 = F(k,x)
+    y2 = F(k,y1)
+
+    return y1 + y2 
+
+def A_2(fn):
+
+    pass
+
+
+from crypto.games.game_prf import GamePRF
+from crypto.simulator.world_sim import WorldSim
+
+if __name__ == '__main__':
+    g_1 = GamePRF(G1, key_len, 2* block_len)
+    s_1 = WorldSim(g_1, A_1)
+
+    g_2 = GamePRF(G2, key_len, 2* block_len)
+    s_2 = WorldSim(g_2, A_2)
+
+    print "The advantage of your adversary A_1 is ~" + \
+                                        str(s_1.compute_advantage())
+
+    print "The advantage of your adversary A_2 is ~" + \
+                                        str(s_2.compute_advantage())
+
